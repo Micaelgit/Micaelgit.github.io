41cf43cdff57d9031f4edb1cc59a8efa1d9e4054
Micael <mikeguedes@gmail.com>
2020-07-28 14:53:04 -0600

    up-to-date

---
+++ projects/javascript_learning/Jonas_Course/js_basics/script.js
@@ -0,0 +1,55 @@
+// var firstName = 'John';
+// console.log(firstName);
+
+// var lastName = 'Smith';
+// var age = 28;
+
+// var fullAge = true;
+// console.log(fullAge);
+
+// var job;
+// console.log(job); 
+
+// job = 'Teacher';
+// console.log(job); 
+
+// var _3years = 3;
+// console.log(_3years);
+
+
+// sdjfsdjflskjdf
+
+/*****************************************
+ * variables mutation and type coertion 
+ */
+
+var firstName = 'John';
+var age = 28;
+
+console.log(firstName + ' ' + age);
+
+var job, isMarried;
+job = 'Teacher';
+isMarried = false;
+
+console.log(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+// variable mutation
+age = 'twenty eight';
+job = 'driver';
+alert(firstName + ' is a ' + age + ' year old ' + job + '. Is he married? ' + isMarried);
+
+var lastName = prompt('what is his last name?');
+console.log(firstName + ' ' + lastName);
+
+/*****************************************
+ * BASIC OPERATORS
+ */
+
+ var year, yearJohn, yearMark;
+ now = 2020;
+ yearJohn = now - 32;
+ yearMark = now - 36;
+
+ console.log(yearJohn);
+ console.log(yearMark);
